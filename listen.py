b79bbaea393cf680fc26ce265a03c3b294131aa5
Ali Shahbaaz Khan <alishahbaazk@gmail.com>
2018-02-03 11:41:49 +0530

    Add files via upload

--- listen.py
+++ listen.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-#!/usr/bin/env python
 
-import pypot.dynamixel
+import pypot.dynamixel 
 import time
 import itertools
 import numpy as np
+import json
 import xml.etree.ElementTree as ET
 import rospy
 from std_msgs.msg import String
@@ -12,42 +12,35 @@ from std_msgs.msg import String
 darwin = {1: 90, 2: -90, 3: 67.5, 4: -67.5, 7: 45, 8: -45, 9: 'i', 10: 'i', 13: 'i', 14: 'i', 17: 'i', 18: 'i'}
 abmath = {11: 15, 12: -15, 13: -10, 14: 10, 15: -5, 16: 5}
 hand = {5: 60, 6: -60}
-
-path1 = "data.xml"
-path2 = "newdata.xml"
-
+path = "/home/ali/catkin/src/walk/scripts/super.json"
 
 class Dynamixel(object) :
-	def __init__(self) :
+	def __init__(self,lock,default_id=0) :
 		ports = pypot.dynamixel.get_available_ports()
 		if not ports :
-			raise IOError("port bhakchodi pel rahe hain")
+			raise IOError("No ports found ")
 
-		print "Is port se judna hai",ports[0]
+		print "Connecting to ",ports[default_id]
 
-		self.dxl = pypot.dynamixel.DxlIO(ports[0])
+		self.dxl = pypot.dynamixel.DxlIO(ports[default_id])
 		self.ids = self.dxl.scan(range(25))
 		print self.ids
 		self.dxl.enable_torque(self.ids)
-		if len(self.ids)<20 :
-			raise RuntimeError("kuch motor bhakchodi pel rahe hain")
-		self.dxl.set_moving_speed(dict(zip(self.ids,itertools.repeat(100))))
+		if len(self.ids)<lock :
+			raise RuntimeError("all the motors were not detected")
+
+		self.dxl.set_moving_speed(dict(zip(self.ids,itertools.repeat(1000))))
 
 
-	def setSpeed(self,speed,ids) :
-		self.dxl.set_moving_speed(dict(zip(ids,itertools.repeat(speed))))
-
-	def setPos(self,pose) :
+	def posWrite(self,pose) :
 		pos = {ids:angle for ids,angle in pose.items()}
 		self.dxl.set_goal_position(pos)
+
 
 	def listWrite(self,list) :
 		pos = dict(zip(self.ids,list))
 		self.dxl.set_goal_position(pos)
 
-	def dictWrite(self,dicti) :
-		
-		self.dxl.set_goal_position(dicti)
 
 	def angleWrite(self,ids,pose) :
 		self.dxl.set_goal_position({ids:pose})
@@ -57,113 +50,176 @@ class Dynamixel(object) :
 		return self.dxl.get_present_position((ids,))	
 
 
-
-class XML(object) :
+class JSON(object) :
 	def __init__(self,file) :
-		try :
-			tree = ET.parse(file)
-			self.root = tree.getroot()
-		except :
-			raise RuntimeError("File nahi mil rahi")
+		#try :
+		with open(file) as f :
+			self.data = json.load(f)
+		#except :
+		#	raise RuntimeError("File not found")
 
 	def parse(self,motion) :
-		find = "PageRoot/Page[@name='" +motion+ "']/steps/step"
-		steps = [x for x in self.root.findall(find)]
 		p_frame = str()
 		p_pose = str()
-		for step in steps :
-			Walk(step.attrib['frame'],step.attrib['pose'],p_frame,p_pose)
-			p_frame = step.attrib['frame']
-			p_pose = step.attrib['pose']
+		write = []
+		json = self.data["Root"]["PageRoot"]["Page"]
+		for j in json :
+			try :
+				if motion in j["name"] :
+					for step in j["steps"]["step"] :
+						write.append(Motion(step["frame"],step["pose"],p_frame,p_pose))
+						p_frame = step["frame"]
+						p_pose = step["pose"]
+			except :
+				raise RuntimeError("Motion not found")
+		return write
 			
+	def setparse(self,motion,offset=[]) :
+		js = self.data["Root"]["FlowRoot"]["Flow"]
+		motionset = []
+		for j in js :
+			try : 
+				if motion in j["name"] :
+					for unit in j["units"]["unit"] :
+						motionset.append(Motionset(json.parse(motion=unit["main"]),speed=float(unit["mainSpeed"])/2,offset=offset))
+			except :
+				raise RuntimeError("Motionset not found")
+
+		return motionset
+
 	
-
-
-class Walk(object) :
+	
+class Motion(object) :
 	def __init__(self,frame,pose,p_frame,p_pose) :
 		self.frame = int(frame)
 		self.begin = {}
 		self.end = {}
+	
 		if not(p_pose) :
 			self.frame_diff = 1
 			p_pose = pose
 		else :
 			self.frame_diff = self.frame-int(p_frame) 
 
+			
 		for ids,pos in enumerate(map(float,p_pose.split())) :
-			self.end[ids+1]=pos	
+			self.end[ids+1] = pos	
 
 		for ids,pos in enumerate(map(float,pose.split())) :
-			self.begin[ids+1]=pos
-		
-		self.set(offsets=[darwin,hand])
+			self.begin[ids+1] = pos
 
-	def Offset(self,offset) :
-		
-		for key in offset.keys() :
-			if offset[key] == 'i' :
-				self.begin[key] = -self.begin[key]
-				self.end[key] = -self.end[key]
-			else :
-				self.begin[key] += offset[key]
-				self.end[key] += offset[key]
-		
-		
 
-	def set(self,offsets=[]) :
-		for offset in offsets :
-			self.Offset(offset)
-		self.motion() 
+	def setoffset(self,offset={},darwin=True) :
+		if not(darwin) :
+			pass
 
-	def motion(self) :
-		write=[]
-		ids=[]
+		else :
+			for key in offset.keys() :
+				if offset[key] == 'i' :
+					self.end[key] = -self.end[key]
+					self.begin[key] = -self.begin[key]
+				else :
+					self.end[key] += offset[key]
+					self.begin[key] += offset[key]		
+
+
+	def motion(self,speed=1) :
+		
+		print self.begin
+		print self.end
+		write = []
+		ids = []
 		for key in self.end.keys() :
-			linp=np.linspace(self.end[key],self.begin[key],self.frame_diff)
+			linp = np.linspace(self.end[key],self.begin[key],self.frame_diff)
 			write.append(linp)
 			ids.append(key)	
 
 		for pose in zip(*write) :
-			x.setPos(dict(zip(ids,pose)))
-			time.sleep(0.0017)
+			print pose
+			dxl.posWrite(dict(zip(ids,pose)))
+			time.sleep(0.008/speed)
 
-def st_pos():
-	w1 = xml1.parse("32 F_S_L")
-	#time.sleep(0.01)
-	w2 = xml1.parse("33 ")
-	#time.sleep(0.01)				
 
-def ft_walk():
-	w3 = xml1.parse("38 F_M_R")
-	#time.sleep(0.01)	
-	w4 = xml1.parse("39 ")
-	#time.sleep(0.01)
-	w5 = xml1.parse("36 F_M_L")
-	#time.sleep(0.01)
-	w6 = xml1.parse("37 ")
-	#time.sleep(0.01)
 
-def bk_walk():
-	w3 = xml2.parse("38 F_M_R")
-	#time.sleep(0.01)	
-	w4 = xml2.parse("39 ")
-	#time.sleep(0.01)
-	w5 = xml2.parse("36 F_M_L")
-	#time.sleep(0.01)
-	w6 = xml2.parse("37 ")
-	#time.sleep(0.01)
+class Motionset(object) :
+	def __init__(self,motion,speed=1,offset=[]) :
+		self.motion = motion
+		self.offset = offset
+		self.speed = speed
+		self.init = 0
+
+	def run(self,speed=1) :
+		speed = self.speed
+		if self.init > 0 :
+			self.exe(speed)
+
+		else :
+			self.init += 1
+			for motion in self.motion :
+				for offset in self.offset :
+					#for m in motion :
+					motion.setoffset(offset)
+				motion.motion(speed)
+			
+	def exe(self,speed) :
+		for motion in self.motion :
+			motion.motion(speed)	
+								
+
+class Custom(object) :
+	def __init__(self,motionset) :
+		self.motionset = motionset
+
+	def run(self) :
+		#prev_motionset = str()
+		for motionset in self.motionset :
+			speed = motionset.speed
+			motionset.run(speed)
+
+		
+
+json = JSON(path)
+balance = Motionset(json.parse(motion="152 Balance"),offset=[darwin,hand])
+w1 = Motionset(json.parse(motion="32 F_S_L"),speed=2.1,offset=[darwin])
+w2 = Motionset(json.parse(motion="33 "),speed=2.1,offset=[darwin])
+w3 = Motionset(json.parse(motion="38 F_M_R"),speed=2.7,offset=[darwin])
+w4 = Motionset(json.parse(motion="39 "),speed=2.1,offset=[darwin])
+w5 = Motionset(json.parse(motion="36 F_M_L"),speed=2.7,offset=[darwin])
+w6 = Motionset(json.parse(motion="37 "),speed=2.1,offset=[darwin])
+l_turn = Motionset(json.parse(motion="27 RT"),speed=1.5,offset=[darwin])
+r_turn = Motionset(json.parse(motion="28 LT"),speed=1.5,offset=[darwin])
+walk_init = Custom(motionset=[w1,w2])
+walk_motion = Custom(motionset=[w3,w4,w5,w6])				
+fast_left = Motionset(json.parse(motion="9 ff_r_l"),speed=1.5,offset=[darwin])
+fast_right = Motionset(json.parse(motion="10 ff_l_r"),speed=1.5,offset=[darwin])
+fast_walk = Custom(motionset=[fast_left,fast_right])
+
 
 def listener(data) :
-	print data.data
+	rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
-	if data.data >12000 :
-		bk_walk()
-	else :
-		ft_walk()
+	if data.data == "Piche":
+		fast_walk.run()
+	elif data.data == "Agey" :
+		back_walk()
+	elif data.data == "daye" :
+		balance.run()
+		time.sleep(1)
+		r_turn.run()
+		time.sleep(0.8)
+	elif data.data == "baye" :
+		balance.run()
+		time.sleep(1)
+		l_turn.run()
+		time.sleep(0.8)
 		
-if __name__ == "__name__":
-	balance = xml1.parse("152 Balance")
+if __name__ == "__main__":
+	dxl = Dynamixel(lock=20)
+	dxl.angleWrite(20,65)
+	
+	balance.run()
 	raw_input("Proceed?")
+		
 	rospy.init_node('Dash', anonymous=True)
     	rospy.Subscriber('get_area',String,listener,queue_size=1)
     	rospy.spin()
--- publish.py
+++ publish.py
@@ -5,75 +5,97 @@ import cv2
 import numpy as np
 import time
 import rospy
+import threading
 from std_msgs.msg import String
 y,u,v = 0,101,80
 
 
 def getArea() :
-	cap = cv2.VideoCapture(1)
+	cap = cv2.VideoCapture(0)
 	'''width,height = cap.get(3),cap.get(4)
 	cap.set(3,width/5)
 	cap.set(4,height/5)'''
-	
+	move = "pappu"
 	flag1=False
 	t=time.time()
 	rec=True
-	while rec:
+	while rec :
+		print "vve"
 		rec,img = cap.read()
 		img_yuv = cv2.cvtColor(img,cv2.COLOR_BGR2YUV)
-	
+		#cv2.imshow(" ",img)
 		blur = cv2.GaussianBlur(img_yuv,(11,11),2)
 		ball = cv2.inRange(blur, (np.array([0,u-30,v-30])), (np.array([255,u+30,v+30])))
-		im_floodfill = ball.copy()
-		h, w = ball.shape[:2]
-		mask = np.zeros((h+2, w+2), np.uint8)
-		cv2.floodFill(im_floodfill, mask, (0,0), 255)
-		fill = cv2.bitwise_and(im_floodfill,im_floodfill,mask = ball)
-	
+		#im_floodfill = ball.copy()
+		#h, w = ball.shape[:2]
+		#mask = np.zeros((h+2, w+2), np.uint8)
+		#cv2.floodFill(im_floodfill, mask, (0,0), 255)
+		#fill = cv2.bitwise_and(im_floodfill,im_floodfill,mask = ball)
+		#cv2.imshow("dfdfd",ball)
 		
-	
-		#images,s_contour,hierarchy = cv2.findContours(crop_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	
-		images,contour,hierarchy = cv2.findContours(fill,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		# images,s_contour,hierarchy = cv2.findContours(crop_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		images,contour,hierarchy = cv2.findContours(ball,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+		print "wvev"
+		cv2.drawContours(ball, contour, -1, (0,255,0), 2)
+
 		
-		cv2.drawContours(img, contour, -1, (0,255,0), 2)
-
-
-
+		#center = (int(x),int(y))
+		#cv2.imshow("dfdfd",ball)
+		if cv2.waitKey(25)==27 & 0xff:
+			break
+	
 
 		if len(contour)>0:
 			cnt = contour[0]
 			area = cv2.contourArea(cnt)
+			(x,y),radius = cv2.minEnclosingCircle(cnt)
 			print area
-			return area
+			
+			if area<20000:
+				move = "Agey"
+				if x>380 :
+					move = "daye"
 
-		else:
-			pass
-		cv2.imshow(" ",img)
-		if cv2.waitKey(1)&0xff==27:
-		    break
+				elif x<260 :
+					move = "baye"
+			
+			else:
+				flag1 = False
+				move = "Piche"
 
+		
+
+		#while not rospy.is_shutdown() :
+		return move
+
+		
 
 
 def talker() :
-   
-        #msg=raw_input()
-        pub=rospy.Publisher('get_area',String,queue_size=10)
-        rospy.init_node('talker',anonymous=True) 
-        rate=rospy.Rate(10)
 
-        while not rospy.is_shutdown() :
-	    msg = getArea() 
-            pub.publish(msg)
-        
+	#msg=raw_input()
+	pub=rospy.Publisher('get_area',String,queue_size=10)
+	rospy.init_node('talker',anonymous=True) 
+	rate=rospy.Rate(20)
+	#thread = threading.Thread(target=getArea)
+	#thread.start()
+	while not rospy.is_shutdown() :
+		msg = getArea() 
+		pub.publish(msg)
+
 
 
 if __name__=="__main__" :
-    try :
-        talker()
+	try :
+		talker()
 
-    except rospy.ROSInterruptException :
-        pass
+	except rospy.ROSInterruptException :
+		cv2.destroyAllWindows()
+		cap.release()
+
+
 
 
 
